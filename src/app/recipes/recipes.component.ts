a1c3c57c1e32833a7ac74a81ec95974b90bb7e83
MSI\jou <michelle22lin@gmail.com>
2025-03-15 18:20:00 +0800

    移除 test component

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { HotRecipesComponent } from './hot-recipes/hot-recipes.component'
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
-import { TestComponent } from "../test/test.component";
 
 
 @Component({
     selector: 'app-recipes',
-    imports: [HotRecipesComponent, RecipesListComponent, TestComponent],
+    imports: [HotRecipesComponent, RecipesListComponent],
     templateUrl: './recipes.component.html',
     styleUrl: './recipes.component.css'
 })
